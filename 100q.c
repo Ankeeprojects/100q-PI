f3b3e0a3ab3057fe23be243d8f67939888f577f0
Pedro António <pmscpa@gmail.com>
2019-04-28 16:19:12 +0100

    Add files via upload

--- 100q.c
+++ 100q.c
@@ -83,10 +83,6 @@ int removeOneOrd (LInt *l, int x){
 
 // QUESTAO 7
 void merge (LInt *r, LInt l1, LInt l2){
-    if (!l2)
-        *r = l1;
-    else if (!l1)
-        *r = l2;
 
     for (;l1 && l2; *r = &(*r)->prox)
         if (l1->valor > l2->valor) {
@@ -97,10 +93,10 @@ void merge (LInt *r, LInt l1, LInt l2){
             l1 = l1->prox;
         }
 
-    if (!l1)
-        *r = l2;
-    else
+    if (l1)
         *r = l1;
+    else
+        *r = l2;
 }
 // QUESTAO 8
 void splitQS (LInt l, int x, LInt *mx, LInt *Mx){
